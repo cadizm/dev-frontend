baeef838070619db8b97b52b0065906a37ca5839
Michael Cadiz <mike@cadizm.com>
2022-09-12 08:37:03 -0700

    Update title on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>cadizm | dev</title>
+      </Head>
+
       <ul className="tracking-widest">
         <li><a href="https://cadizm.com/scramble/">Scramble</a></li>
         <li><a href="/words">Words</a></li>
